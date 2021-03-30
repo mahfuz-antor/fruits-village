814c2aed483fd15919d8872ad8336830055c10b1
mahfuz-antor <antorcnj2014@gmail.com>
2021-03-31 01:45:24 +0600

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,40 @@
-import logo from './logo.svg';
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route
+} from "react-router-dom";
+import Home from './Components/Home/Home';
+import Admin from './Components/Admin/Admin';
+import Login from './Components/Login/Login';
+import Orders from './Components/Orders/Orders';
+import Header from './Components/Header/Header';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Switch>
+        <Route path="/home">
+          <Home></Home>
+        </Route>
+        <Route exact path="/">
+          <Home></Home>
+        </Route>
+        <Route path="/header">
+          <Header></Header>
+        </Route>
+        <Route path="/admin">
+          <Admin></Admin>
+        </Route>
+        <Route path="/orders">
+          <Orders></Orders>
+        </Route>
+        <Route path="/login">
+          <Login></Login>
+        </Route>
+      </Switch>
+    </Router>
   );
 }
 
---
+++ src/Components/Login/Login.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Header from '../Header/Header';
+
+const Login = () => {
+    return (
+        <div>
+            <Header></Header>
+            <h1>This is Login Page.</h1>
+        </div>
+    );
+};
+
+export default Login;
